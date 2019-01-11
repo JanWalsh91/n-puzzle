b74760acca7bc0dcca7898ebe81d7abc3284ed29
Jan WALSH <jwalsh@e3r3p12.42.fr>
2019-01-11 11:24:06 +0100

    first push

---
+++ server/src/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace server.src {
+
+	public class Server {
+		
+		static void Main() {
+
+			Console.WriteLine("Hello World!");
+
+			// Keep the console window open in debug mode.
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+
+		}
+	}
+
+}
